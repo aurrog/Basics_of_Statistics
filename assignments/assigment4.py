b4347b6f88500d821d8127346bd836a130f40f96
aurrog <gnusovme@gmail.com>
2022-07-15 10:57:33 +0300

    refactor assignment4.py

--- assignments/assigment4.py
+++
@@ -1,9 +0,0 @@
-import math
-
-n = 144
-sd = 9
-mean = 118
-
-se=sd/math.sqrt(n)
-z = (x-mean)
-
---
+++ assignments/assignment4.py
@@ -0,0 +1,29 @@
+import math
+import random
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def mean(a):
+    return sum(a) / len(a)
+
+
+x = np.arange(0, 100)
+y2 = [random.choices(x, k=10) for i in range(100)]
+y = []
+for i in y2:
+    y.append(mean(i))
+
+random.shuffle(y)
+print(mean(x))
+print(mean(y))
+
+fig, ax = plt.subplots()
+
+ax.bar(x, y)
+plt.plot(x, y, c='red')
+plt.plot(x, [i/2 for i in y], c='black')
+plt.scatter(2, mean(x), c='green', s=150)
+
+plt.show()
